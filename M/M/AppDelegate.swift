da00206007905ca00621377cc1d70ff2f3b93764
Yangsibai <yangsibai@gmail.com>
2016-09-22 10:51:35 +0800

    update

---
+++ M/M/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  M
+//
+//  Created by yangsibai on 15/2/24.
+//  Copyright (c) 2015年 massimo. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
